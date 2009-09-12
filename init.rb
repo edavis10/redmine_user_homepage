628ca3ba1299d44a22c2819c1628018c27cc9b70
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-11 17:58:55 -0700

    [#3015] Updated the init.rb documentation

--- init.rb
+++ init.rb
@@ -1,8 +1,12 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_user_homepage do
-  name 'Redmine User Homepage plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  name 'User Homepage'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-misc'
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'User Homepage is a plugin to set the Redmine homepage to be the Project Overview for specific Roles.'
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.8.0'
 end
