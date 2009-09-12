cefb062635cbac02486feabaa84fde76e91b9aa2
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-11 17:43:03 -0700

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_user_homepage do
+  name 'Redmine User Homepage plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
